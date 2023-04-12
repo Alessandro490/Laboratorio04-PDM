47ff82ea6af7a388f6b96e7855248eea591645a4
Alessandro490 <00101519@uca.edu.sv>
2023-04-12 12:59:45 -0600

    add share funcionality

--- app/src/main/java/com/example/laboratorio04rodriguez/ui/SecondActivity.kt
+++ app/src/main/java/com/example/laboratorio04rodriguez/ui/SecondActivity.kt
@@ -1,7 +1,9 @@
 package com.example.laboratorio04rodriguez.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.laboratorio04rodriguez.R
 
@@ -9,6 +11,7 @@ class SecondActivity : AppCompatActivity() {
     private lateinit var nameTextView: TextView
     private lateinit var emailTextView: TextView
     private lateinit var numberTextView: TextView
+    private lateinit var actionshare: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,7 @@ class SecondActivity : AppCompatActivity() {
         nameTextView =findViewById(R.id.resultTextView)
         emailTextView = findViewById(R.id.EmailResultTextView)
         numberTextView= findViewById(R.id.NumberResultTextView)
+        actionshare = findViewById(R.id.ShareBUtton)
 
         val nameShared = intent.getStringExtra(MainActivity.Share_Name).toString()
         nameTextView.text = "Nombre: ${nameShared}"
@@ -26,5 +30,16 @@ class SecondActivity : AppCompatActivity() {
 
         val numberShared = intent.getStringExtra(MainActivity.Share_Number).toString()
         numberTextView.text = "Número telefónico: ${numberShared}"
+
+        actionshare.setOnClickListener {
+            val sendIntent = Intent()
+            sendIntent.action = Intent.ACTION_SEND
+            sendIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Hey Check out this Great app:"
+            )
+            sendIntent.type = "text/plain"
+            startActivity(Intent.createChooser(sendIntent, "compartir"))
+        }
     }
 }
